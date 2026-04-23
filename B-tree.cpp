a4034e425841de7e3fc70ac15b07e9eed1311a21
ToombaYoomba <z.morkhov@gmail.com>
2026-04-23 21:58:39 +0300

    Finished deletion and finding methods for B-tree

--- B-tree.cpp
+++ B-tree.cpp
@@ -12,6 +12,10 @@ public:
     Token(): key(""), value(0) {}
 
     Token(string k, float v): key(k), value(v) {}
+
+    bool operator==(const Token& other) const {
+        return key == other.key;
+    }
 };
 
 bool compareTokens(const Token& a, const Token& b) {
@@ -24,9 +28,14 @@ public:
     vector<Token> tokens;
     vector<Node*> children;
 
-    Node(vector<Token> t): parent (nullptr), tokens(t), children(4, nullptr) {} 
+    Node(): parent(nullptr), tokens(), children() {}
+
+    Node(vector<Token> t): parent(nullptr), tokens(t), children() {} 
 
     bool Allnullptr(){
+        if (children.size() == 0){
+            return true;
+        }
         for (auto* ptr : children) {
             if (ptr != nullptr) {
                 return false;
@@ -54,12 +63,150 @@ void rewireNode(Node* new_parent_Node, Node* new_child_Node, int direction){
 }
 
 class BinaryTree { // по сути имеет root ноду и набор методов
+private:
+    pair<Node*, Token> findNodeAndToken(Node* curr_node, string str){
+        if (curr_node == nullptr) {
+            return {nullptr, Token()};
+        }
+
+        for (int i = 0; i < curr_node->tokens.size(); i++){
+            if (curr_node->tokens[i].key == str){
+                return {curr_node, curr_node->tokens[i]};
+            }
+        }
+
+        for (int i = 0; i < curr_node->children.size(); i++){
+            auto [found_node, found_token] = findNodeAndToken(curr_node->children[i], str);
+            if (found_node != nullptr){
+                return {found_node, found_token};
+            }
+        }
+
+        return {nullptr, Token()};
+    }
+
+    pair<Node*, Token> findNodeAndToken(string str){
+        return findNodeAndToken(root, str);
+    }
+
+    pair<Node*, Token> findMinSuccessor(Node* del_node, Token del_token){
+        auto it = find(del_node->tokens.begin(), del_node->tokens.end(), del_token);
+        int index = distance(del_node->tokens.begin(), it);
+
+        Node* curr_node = del_node->children[index+1];
+
+        while(1){
+            if (!curr_node->Allnullptr()){
+                curr_node = curr_node->children[0];
+            }
+            else{
+                return {curr_node, curr_node->tokens[0]};
+            }
+        }
+    }
 public:
     Node* root;
 
     BinaryTree(Node* starting_node) : root(starting_node) {}
 
-    void addNode(Token tok){ // передаю токен а не значение (будет в мейне)
+    Token findToken(Node* curr_node, string str){
+        if (curr_node == nullptr) {
+            return Token();
+        }
+
+        for (int i = 0; i < curr_node->tokens.size(); i++){
+            if (curr_node->tokens[i].key == str){
+                return curr_node->tokens[i];
+            }
+        }
+
+        for (int i = 0; i < curr_node->children.size(); i++){
+            Token curr_result = findToken(curr_node->children[i], str);
+            if (curr_result.key != ""){
+                return curr_result;
+            }
+        }
+
+        return Token();
+    }
+
+    Token findToken(string str){
+        return findToken(root, str);
+    }
+
+
+
+    void delToken(string str){
+        auto [del_node, del_token] = findNodeAndToken(str);
+        auto it = find(del_node->tokens.begin(), del_node->tokens.end(), del_token);
+        int index = distance(del_node->tokens.begin(), it);
+
+        if (del_node->Allnullptr()){ // нет детей
+            if (del_node->tokens.size() > 1){ // не единственный в узле
+                auto it = find(del_node->tokens.begin(), del_node->tokens.end(), del_token);
+                del_node->tokens.erase(it);
+                return;
+            }
+            // единственный в корне
+            if (del_node == root){
+                root = new Node();
+                delete del_node;
+                return;
+            }
+            // лист не в корне
+            
+            auto it = find(del_node->parent->children.begin(), del_node->parent->children.end(), del_node);
+            int direction = distance(del_node->parent->children.begin(), it);
+            // cout << direction << endl;
+            if (direction - 1 > 0){ // если берём у левого
+                if (del_node->parent->children[direction-1]->tokens.size() > 1){
+                    Token new_tok = del_node->parent->tokens[direction-1];
+                    Token new_replace_tok = del_node->parent->children[direction-1]->tokens[-1];
+                    del_node->parent->tokens[direction-1] = new_replace_tok;
+                    del_node->tokens[0] = new_tok;
+                    del_node->parent->children[direction-1]->tokens.pop_back();
+                    return;
+                }
+            }
+            if (direction + 1 < del_node->parent->children.size()){ // если берём у правого
+                if (del_node->parent->children[direction+1]->tokens.size() > 1){
+                    Token new_tok = del_node->parent->tokens[direction];
+                    Token new_replace_tok = del_node->parent->children[direction+1]->tokens[0];
+                    del_node->parent->tokens[direction] = new_replace_tok;
+                    del_node->tokens[0] = new_tok;
+                    del_node->parent->children[direction+1]->tokens.erase(del_node->parent->children[direction+1]->tokens.begin());
+                    return;
+                }
+            }
+            
+            // если слияние
+            if (direction - 1 > 0){ // если сливаем с левым
+                Token new_add_tok = del_node->parent->tokens[direction-1];
+                del_node->parent->children[direction-1]->tokens.push_back(new_add_tok);
+                auto it = find(del_node->parent->tokens.begin(), del_node->parent->tokens.end(), new_add_tok);
+                del_node->tokens.erase(it);
+                del_node->parent->children.erase(del_node->parent->children.begin() + direction);
+                delete del_node;
+                return;
+            }
+            if (direction + 1 < del_node->parent->children.size()){ // если сливаем с правым
+                Token new_add_tok = del_node->parent->tokens[direction];
+                del_node->parent->children[direction+1]->tokens.insert(del_node->parent->children[direction+1]->tokens.begin(), new_add_tok);
+                auto it = find(del_node->parent->tokens.begin(), del_node->parent->tokens.end(), new_add_tok);
+                del_node->parent->tokens.erase(it);
+                del_node->parent->children.erase(del_node->parent->children.begin() + direction);
+                delete del_node;
+                return;
+            }
+        }
+        // есть дети
+        auto [successor_node, successor_token] = findMinSuccessor(del_node, del_token);
+        delToken(successor_token.key);
+        del_node->tokens[index] = successor_token;
+        return;
+    }
+
+    void addToken(Token tok){ // передаю токен а не значение (будет в мейне)
         if (root == nullptr){ // если дерево пустое
             Node* nod = new Node(vector<Token> {tok});
             root = nod;
@@ -75,14 +222,20 @@ public:
                         direction = i;
                     }
                 }
+                // cout << direction << endl;
                 if (curr_Node->children[direction] != nullptr){
                     curr_Node = curr_Node->children[direction]; // перешли на ребёнка
                 }
+                // else{
+                //     cout << "ERROR" << endl;
+                //     return;
+                // }
                 continue;
             }
             else{ // нет детей
                 curr_Node->tokens.push_back(tok); // добавление токена в нужное место
                 sort(curr_Node->tokens.begin(), curr_Node->tokens.end(), compareTokens);
+                break;
             }
 
         }
@@ -129,7 +282,7 @@ public:
                 curr_Node = par;
                 continue;     
             }
-            else{
+            else{ // переполнение без детей (у листов)
                 vector<Token> left(curr_Node->tokens.begin(), curr_Node->tokens.begin() + 2);
                 vector<Token> right = {curr_Node->tokens[3]};
                 Token middle = curr_Node->tokens[2];
@@ -138,7 +291,9 @@ public:
                     Node* new_root = new Node(vector<Token> {middle});
                     root = new_root;
                     root->children[0] = new Node(left);
+                    root->children[0]->parent = root;
                     root->children[1] = new Node(right);
+                    root->children[1]->parent = root;
                     delete curr_Node;
                     return;
                 }
@@ -179,3 +334,71 @@ public:
     }
 
 };
+
+int main(){
+    Token a = Token("a", 1);
+    Token b = Token("b", 2);
+    Token c = Token("c", 3);
+    Token d = Token("d", 4);
+    Token e = Token("e", 5);
+    Token f = Token("f", 6);
+    Token g = Token("g", 7);
+    Token h = Token("h", 8);
+
+    Node* f_node = new Node(vector<Token> {b, g});
+    BinaryTree tree = BinaryTree(f_node);
+
+    Node* one_node = new Node(vector<Token> {a});
+    Node* two_node = new Node(vector<Token> {c, d, e});
+    Node* three_node = new Node(vector<Token> {h});
+
+    tree.root->addChild(one_node);
+    tree.root->addChild(two_node);
+    tree.root->addChild(three_node);
+
+    tree.printTree(tree.root);
+
+    string need = "e";
+
+    Token found = tree.findToken(need);
+    cout << found.value << endl;
+
+    tree.addToken(f);
+
+    tree.printTree(tree.root);
+
+    tree.delToken("d");
+
+    tree.printTree(tree.root);
+
+    tree.delToken("b");
+
+    tree.printTree(tree.root);
+
+
+    // cout << tree.root->tokens[0].key << endl;
+
+    // // if (tree.root->Allnullptr()){
+    // //     cout << 1 << endl;
+    // // }
+    // // else{
+    // //     cout << 0 << endl;
+    // // }
+
+    // tree.addToken(b);
+
+    // cout << tree.root->tokens[1].key << endl;
+    // tree.printTree(tree.root);
+
+    // tree.addToken(c);
+    // tree.addToken(d);
+
+    // tree.printTree(tree.root);
+
+
+
+
+
+
+    return 0;
+}
